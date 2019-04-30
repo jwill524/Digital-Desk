3646b0b4e71cf7c5e4dc4b0c8bb92f7e7f8bbc41
Jonathan Williams <jonathanwilliams96@hotmail.com>
2019-04-30 14:24:19 -0400

    Update App.js

--- digital-desk/src/App.js
+++ digital-desk/src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import * as firebase from 'firebase'
+// import * as firebase from 'firebase'
 
-let config = {
-  apiKey: "AIzaSyB-naxDMgSOMVVBv7nwrMcE8UzIsbfHafI",
-  authDomain: "team3desk.firebaseapp.com",
-  databaseURL: "https://team3desk.firebaseio.com",
-  projectId: "team3desk",
-  storageBucket: "team3desk.appspot.com",
-  messagingSenderId: "244310026921"
-};
+// let config = {
+//   apiKey: "AIzaSyB-naxDMgSOMVVBv7nwrMcE8UzIsbfHafI",
+//   authDomain: "team3desk.firebaseapp.com",
+//   databaseURL: "https://team3desk.firebaseio.com",
+//   projectId: "team3desk",
+//   storageBucket: "team3desk.appspot.com",
+//   messagingSenderId: "244310026921"
+// };
 
-firebase.initializeApp(config);
+// firebase.initializeApp(config);
 
-const database = firebase.database();
+// const database = firebase.database();
 
 class App extends React.Component {
   constructor(){
@@ -25,18 +25,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          
         </header>
       </div>
     );
